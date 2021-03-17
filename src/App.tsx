0cd9e90352aba92f4f813520c8693cb47096adfd
Joao Azevedo <devjoaoazevedo@gmail.com>
2021-03-17 10:37:16 -0300

    Add project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyled = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+   body {
+     background: #000;
+   }
+`;
+
+export default GlobalStyled;
